321523021d3fe005aa94eab2056a2285113f6627
Ricky <ricky2011@knights.ucf.edu>
2016-05-10 21:00:21 -0400

    Added LogConnection(3) and LogLoginAttemp(3).

--- BlueSOD/ServerManager.cpp
+++ BlueSOD/ServerManager.cpp
@@ -8,6 +8,7 @@ extern std::shared_mutex serverMutex;
 extern std::shared_mutex serverStateMutex;
 
 using std::string;
+using std::fstream;
 
 ServerManager::~ServerManager()
 {
@@ -229,6 +230,7 @@ SOCKET ServerManager::CreateSocket(int port)
 	addr.sin_addr.S_un.S_addr = ADDR_ANY;
 	addr.sin_port = htons(port);
 	
+	//Create socket.
 	socket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (socket == INVALID_SOCKET)
 	{
@@ -239,6 +241,7 @@ SOCKET ServerManager::CreateSocket(int port)
 		return INVALID_SOCKET;
 	}
 
+	//Bind socket.
 	if (bind(socket, (struct sockaddr*)&addr, sizeof(addr)) == INVALID_SOCKET)
 	{
 		string error = string();
@@ -249,6 +252,7 @@ SOCKET ServerManager::CreateSocket(int port)
 		return INVALID_SOCKET;
 	}
 
+	//Listen on socket.
 	if (listen(socket, 0) == INVALID_SOCKET)
 	{
 		string error = string();
@@ -266,6 +270,7 @@ SSL_CTX* ServerManager::CreateSSLContext()
 {
 	SSL_CTX* ctx;
 
+	//Create the SSL context using the Lv2 and Lv3 methods.
 	ctx = SSL_CTX_new(SSLv23_server_method());
 	if (!ctx)
 	{
@@ -278,12 +283,16 @@ SSL_CTX* ServerManager::CreateSSLContext()
 
 bool ServerManager::ConfigureSSLContext()
 {
+	//Path to the certificate.
 	string cPath = string(SSL_PEM_LOCATIONS);
 	cPath += CERTIFICATE_FILE;
+	//Path to the private key.
 	string pkPath = string(SSL_PEM_LOCATIONS);
 	pkPath += PRIVATE_KEY_FILE;
 
+	//I'm not sure what this does, I just know it has to be done.
 	SSL_CTX_set_ecdh_auto(m_sslContext, 1);
+	//Tell OpenSSL where to find our certificate.
 	if (SSL_CTX_use_certificate_file(m_sslContext, cPath.c_str(), SSL_FILETYPE_PEM) < 0)
 	{
 		string errLoc = string(SSL_ERROR_FILE_LOCATION);
@@ -292,7 +301,9 @@ bool ServerManager::ConfigureSSLContext()
 
 		return false;
 	}
+	//Tell OpenSSL where to find our password.
 	SSL_CTX_set_default_passwd_cb(m_sslContext, PasswordCallBack);
+	//Tell OpenSSL where to find our private key.
 	if (SSL_CTX_use_PrivateKey_file(m_sslContext, pkPath.c_str(), SSL_FILETYPE_PEM) < 0)
 	{
 		string errLoc = string(SSL_ERROR_FILE_LOCATION);
@@ -307,10 +318,12 @@ bool ServerManager::ConfigureSSLContext()
 
 bool ServerManager::InitOpenSSL()
 {
+	//Load the OpenSSL library.
 	SSL_load_error_strings();
 	ERR_load_BIO_strings();
 	OpenSSL_add_ssl_algorithms();
 
+	//Create the SSL context.
 	m_sslContext = CreateSSLContext();
 	if (m_sslContext == nullptr)
 	{
@@ -319,10 +332,12 @@ bool ServerManager::InitOpenSSL()
 		LogSSLError(fileLoc);
 		return m_bOpenSSL = false;
 	}
+	//Configure the SSL context.
 	return m_bOpenSSL = ConfigureSSLContext();
 }
 void ServerManager::CleanupOpenSSL()
 {
+	//OpenSSL wasn't initialized, so this function does nothing.
 	if (!m_bOpenSSL)
 		return;
 	SSL_CTX_free(m_sslContext);
@@ -332,8 +347,10 @@ void ServerManager::CleanupOpenSSL()
 bool ServerManager::InitWSA()
 {
 	WSADATA wsaData;
+	//Start up WSA.
 	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+	//There was an error with WSA. Might be best to throw an error here?
 	if (result != 0)
 	{
 		string error = string();
@@ -355,6 +372,7 @@ void ServerManager::CleanupWSA()
 void ServerManager::LogSSLError(const std::string& fileName)
 {
 	FILE* err = fopen(fileName.c_str(), "w");
+	//Log the OpenSSL error.
 	ERR_print_errors_fp(err);
 	fclose(err);
 }
@@ -362,28 +380,50 @@ void ServerManager::LogSSLError(const std::string& fileName)
 void ServerManager::LogError(const std::string& fileName, const std::string& errorMessage)
 {
 	FILE* err = fopen(fileName.c_str(), "w");
+	//Log the error. Might be best to change this to C++ style to avoid any future issues.
 	fputs(errorMessage.c_str(), err);
 	fclose(err);
 }
 
-int ServerManager::PasswordCallBack(char * buffer, int sizeOfBuffer, int rwflag, void * data)
+void ServerManager::LogConnection(const std::string & fileName, const std::string & message, int ip)
 {
-	int dirLen;
-	char* fileName;
+	fstream file = fstream(fileName, std::ios_base::in);
+	string fullMessage = string(message);
+	fullMessage += " from IP:" + ip;
+	
+	if (file.is_open())
+	{
+		file.write(fullMessage.c_str(), fullMessage.size() + 1);
+		file.close();
+	}
+}
+
+void ServerManager::LogLoginAttemp(const std::string & fileName, const std::string & user, bool successful)
+{
+	fstream file = fstream(fileName, std::ios_base::in);
+	string message = string(successful ? "Successful " : "Unsuccessful ");
+	message += " login attemp by " + user;
+
+	if (file.is_open())
+	{
+		file.write(message.c_str(), message.size() + 1);
+		file.close();
+	}
+}
+
+int ServerManager::PasswordCallBack(char* buffer, int sizeOfBuffer, int rwflag, void* data)
+{
 	FILE* file;
+	//Load the name of the password file.
+	string fName = string(SSL_PASSWORD_FILE_LOCATION);
+	fName += PASSWORD_FILE;
 
-	dirLen = strlen(SSL_PASSWORD_FILE_LOCATION) + strlen(PASSWORD_FILE) + 1;
-	fileName = new char[dirLen];
-	strcat(fileName, SSL_PASSWORD_FILE_LOCATION);
-	strcat(fileName, PASSWORD_FILE);
-	fileName[dirLen - 1] = '\0';
-
-	file = fopen(fileName, "r");
+	file = fopen(fName.c_str(), "r");
+	//Get the password.
 	fgets(buffer, sizeOfBuffer - 2, file);
 	buffer[sizeOfBuffer - 1] = '\0';
-
+	
 	fclose(file);
-	delete[] fileName;
 
 	return strlen(buffer);
 }
