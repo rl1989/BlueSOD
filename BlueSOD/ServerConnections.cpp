3d509b36f81535de415b57d61356c35fd9deec32
Ricky <ricky2011@knights.ucf.edu>
2016-05-31 10:49:49 -0400

    Renamed.

---
+++ BlueSOD/ServerConnections.cpp
@@ -0,0 +1,220 @@
+#include "ServerConnections.h"
+
+Buffer::Buffer()
+	:bytesSent{},
+	bytesRecv{}
+{
+	buffer.buf = new CHAR[BUFFER_SIZE];
+	buffer.len = BUFFER_SIZE;
+}
+
+Buffer::Buffer(const Buffer& ref)
+	: bytesSent{ref.bytesSent},
+	bytesRecv{ref.bytesRecv}
+{
+	buffer.len = ref.buffer.len;
+	buffer.buf = new CHAR[buffer.len];
+	for (int i = 0; i < buffer.len; i++)
+		buffer.buf[i] = ref.buffer.buf[i];
+}
+
+Buffer& Buffer::operator=(const Buffer& ref)
+{
+	if (buffer.buf != nullptr)
+		delete[] buffer.buf;
+	buffer.len = ref.buffer.len;
+	buffer.buf = new CHAR[buffer.len];
+	for (int i = 0; i < buffer.len; i++)
+		buffer.buf[i] = ref.buffer.buf[i];
+
+	return *this;
+}
+
+Buffer::Buffer(Buffer&& move)
+	: bytesRecv{move.bytesRecv},
+	bytesSent{move.bytesSent}
+{
+	buffer.len = move.buffer.len;
+	buffer.buf = move.buffer.buf;
+	
+	move.buffer.buf = nullptr;
+	move.buffer.len = 0;
+	move.bytesRecv = 0;
+	move.bytesSent = 0;
+}
+
+Buffer& Buffer::operator=(Buffer&& move)
+{
+	if (buffer.buf != nullptr)
+		delete[] buffer.buf;
+
+	bytesRecv = move.bytesRecv;
+	bytesSent = move.bytesSent;
+	buffer.buf = move.buffer.buf;
+	buffer.len = move.buffer.len;
+
+	move.bytesRecv = 0;
+	move.bytesSent = 0;
+	move.buffer.len = 0;
+	move.buffer.buf = nullptr;
+
+	return *this;
+}
+
+Buffer::~Buffer()
+{
+	if (buffer.buf)
+		delete[] buffer.buf;
+}
+
+ConnectionInfo::ConnectionInfo()
+	: buffer{ new Buffer{} },
+	connection{},
+	verified{false},
+	connStatus{ ConnectionStatus::CONNECTION_ERROR }
+{}
+
+ConnectionInfo::ConnectionInfo(const Connection& ref)
+	: connection{ref.socket, ref.ssl, ref.address},
+	buffer{ new Buffer{} },
+	verified{false}
+{}
+
+ConnectionInfo::ConnectionInfo(Connection&& move)
+	: buffer{new Buffer{}},
+	connection{ move.socket, move.ssl, move.address },
+	verified{false}
+{
+	move.socket = INVALID_SOCKET;
+	move.ssl = nullptr;
+	move.address = 0;
+}
+
+ConnectionInfo& ConnectionInfo::operator=(const Connection& ref)
+{
+	if (connection == ref)
+		return *this;
+	
+	connection = ref;
+
+	return *this;
+}
+
+ConnectionInfo& ConnectionInfo::operator=(Connection&& move)
+{
+	connection = move;
+
+	move.socket = INVALID_SOCKET;
+	move.ssl = nullptr;
+	move.address = 0;
+
+	return *this;
+}
+
+ConnectionInfo::ConnectionInfo(const ConnectionInfo& ref)
+	: connection{ref.connection.socket, ref.connection.ssl, ref.connection.address},
+	verified{ref.verified},
+	buffer{ref.buffer},
+	connStatus{ref.connStatus}
+{
+}
+
+ConnectionInfo::ConnectionInfo(ConnectionInfo&& move)
+	: connection{move.connection.socket, move.connection.ssl, move.connection.address},
+	buffer{move.buffer},
+	verified{move.verified},
+	connStatus{move.connStatus}
+{
+	move.connection.socket = INVALID_SOCKET;
+	move.connection.ssl = nullptr;
+	move.connection.address = 0;
+	move.buffer = nullptr;
+	move.connStatus = ConnectionStatus::CONNECTION_NOT_INITIATED;
+}
+
+ConnectionInfo& ConnectionInfo::operator=(const ConnectionInfo& ref)
+{
+	if (*this == ref)
+		return *this;
+
+	connection.socket = ref.connection.socket;
+	connection.ssl = ref.connection.ssl;
+	connection.address = ref.connection.address;
+	buffer = ref.buffer;
+	verified = ref.verified;
+	connStatus = ref.connStatus;
+
+	return *this;
+}
+
+ConnectionInfo& ConnectionInfo::operator=(ConnectionInfo&& move)
+{
+	connection = move.connection;
+	buffer = std::move(move.buffer);
+	verified = move.verified;
+	connStatus = move.connStatus;
+
+	move.connection.socket = INVALID_SOCKET;
+	move.connection.ssl = nullptr;
+	move.connection.address = 0;
+	move.connStatus = ConnectionStatus::CONNECTION_NOT_INITIATED;
+
+	return *this;
+}
+
+bool ConnectionInfo::operator==(const ConnectionInfo & ref)
+{
+	return (connection == ref.connection && buffer->buffer.buf == ref.buffer->buffer.buf
+		&& buffer->buffer.len == ref.buffer->buffer.len && verified == ref.verified 
+		&& connStatus == ref.connStatus);
+}
+
+bool Connection::operator==(const Connection& ref)
+{
+	return (socket == ref.socket && ssl == ref.ssl && address == ref.address);
+}
+
+ConnectionInfo* ReadFromSSL(ConnectionInfo* ci, int length)
+{
+	SSL* ssl = ci->connection.ssl;
+	if (ssl == nullptr)
+	{
+		ci->connStatus = ConnectionStatus::SSL_NOT_INITIATED;
+		return ci;
+	}
+
+	char* buffer = ci->buffer->buffer.buf;
+	int res = SSL_read(ssl, buffer, length);
+	if (res <= 0)
+	{
+		LogManager::LogSSLError();
+		ci->connStatus = ConnectionStatus::SSL_ERROR;
+		return ci;
+	}
+	ci->buffer->bytesRecv = res;
+	ci->connStatus = ConnectionStatus::SSL_READ;
+
+	return ci;
+}
+
+ConnectionInfo* SendToSSL(ConnectionInfo* ci, int length)
+{
+	SSL* ssl = ci->connection.ssl;
+	if (ssl == nullptr)
+	{
+		ci->connStatus = ConnectionStatus::SSL_NOT_INITIATED;
+		return ci;
+	}
+
+	char* buffer = ci->buffer->buffer.buf;
+	int res = SSL_write(ssl, buffer, length);
+	if (res <= 0)
+	{
+		ci->connStatus = ConnectionStatus::SSL_ERROR;
+		LogManager::LogSSLError();
+		return ci;
+	}
+	ci->connStatus = ConnectionStatus::SSL_SENT;
+
+	return ci;
+}
---
+++ BlueSOD/ServerConnections.h
@@ -0,0 +1,97 @@
+#pragma once
+#include <WinSock2.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <memory>
+#include "LogManager.h"
+
+using std::shared_ptr;
+using std::unique_ptr;
+using std::move;
+
+//The maximum size of a message a client can send.
+#define BUFFER_SIZE 1024
+//Defines the state of the server. Names are self explanatory.
+enum ServerState
+{
+	OFF, RUNNING, NOT_ACCEPTING_CONNECTIONS, RESET
+};
+
+enum ConnectionStatus
+{
+	CONNECTION_OK,
+	CONNECTION_ACCEPTED,
+	CONNECTION_ERROR,
+	NO_DATA_PRESENT,
+	CONNECTION_NOT_INITIATED,
+	SSL_OK,
+	SSL_SENT = SSL_OK,
+	SSL_READ = SSL_OK,
+	SSL_ERROR,
+	SSL_NOT_INITIATED
+};
+
+struct Buffer
+{
+	WSABUF buffer;
+	DWORD bytesSent;
+	DWORD bytesRecv;
+
+	Buffer();
+	Buffer(const Buffer& ref);
+	Buffer& operator=(const Buffer& ref);
+	Buffer(Buffer&& move);
+	Buffer& operator=(Buffer&& move);
+	~Buffer();
+};
+
+struct Connection
+{
+	SOCKET socket;
+	SSL* ssl;
+	ULONG address;
+
+	bool operator==(const Connection& ref);
+	bool operator==(Connection& ref);
+};
+
+struct ConnectionInfo
+{
+	shared_ptr<Buffer> buffer;
+	Connection connection;
+	bool verified;
+	ConnectionStatus connStatus;
+
+	ConnectionInfo();
+	ConnectionInfo(const Connection& ref);
+	ConnectionInfo(Connection&& move);
+	ConnectionInfo& operator=(const Connection& ref);
+	ConnectionInfo& operator=(Connection&& move);
+	ConnectionInfo(const ConnectionInfo& ref);
+	ConnectionInfo(ConnectionInfo&& move);
+	ConnectionInfo& operator=(const ConnectionInfo& ref);
+	ConnectionInfo& operator=(ConnectionInfo&& move);
+	~ConnectionInfo()=default;
+
+	bool operator==(const ConnectionInfo& ref);
+	bool operator==(ConnectionInfo& ref);
+};
+
+/*
+	Read from the SSL connection. The message is stored in buffer which must be of size BUFFER_SIZE.
+	Alternatively, you can tell the function where in the buffer to store the information. If the amount
+	of data to be read is more than length-start, then the data is thrown away. If buffer is nullptr, then
+	this function throws away the data that would have been stored there.
+
+	The status of the connection is stored in ci.connStatus.
+*/
+[[noreturn]]
+ConnectionInfo* ReadFromSSL(ConnectionInfo* ci, int length = BUFFER_SIZE);
+
+/*
+	Send a message to an SSL connection.
+
+	The status of the connection is stored in ci.connStatus;
+*/
+[[noreturn]]
+ConnectionInfo* SendToSSL(ConnectionInfo* ci, int length = BUFFER_SIZE);
