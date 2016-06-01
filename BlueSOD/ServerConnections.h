446ff946ef40e79ae4bc7e0b4825ea19dd2bae15
Ricky <ricky2011@knights.ucf.edu>
2016-06-01 12:24:15 -0400

    Added ServerStatus::START_UP.

--- BlueSOD/ServerConnections.h
+++ BlueSOD/ServerConnections.h
@@ -10,7 +10,7 @@
 //Defines the state of the server. Names are self explanatory.
 enum ServerState
 {
-	OFF, RUNNING, NOT_ACCEPTING_CONNECTIONS, RESET
+	OFF, RUNNING, NOT_ACCEPTING_CONNECTIONS, RESET, START
 };
 
 enum ConnectionStatus
