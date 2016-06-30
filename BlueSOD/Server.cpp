43bf5e4da5061157d9bed5d0f081d9d5f279bb24
Ricky <ricky2011@knights.ucf.edu>
2016-06-30 02:41:48 -0400

    Many changes. Created a vector of pairs (ClientAndMessageDeque) of a ClientInfo and a string deque object. This holds the messages needing to be sent to the client. And other related functions and DB modification functions.

--- BlueSOD/Server.cpp
+++ BlueSOD/Server.cpp
@@ -6,38 +6,333 @@ using std::mutex;
 using std::string;
 using std::move;
 using std::list;
+using std::deque;
+using std::vector;
 
-#define CONVERSATIONS_IN_VIEW 8
+#define COMMON_TABLE_NAME "Messages"
+#define TO_COLUMN         "To"
+#define FROM_COLUMN       "From"
+#define MSG_COLUMN        "Message"
+#define DATE_COLUMN       "Date"
+#define STATUS_COLUMN     "Status"
+#define UNSENT_STATUS     "Unsent"
 
-//Used for FindSortedPosition when inserting new information into m_clientList.
-#define USERNAME_IN_LIST -1
-
-extern const string RESET_CONNECTION;
+#define TO_COL_POS     0
+#define FROM_COL_POS   1
+#define MSG_COL_POS    2
+#define DATE_COL_POS   3
+#define STATUS_COL_POS 4
 
 void Server::DisconnectClients()
 {
+	lock_guard<mutex> lck(m_pairMutex);
 	/*Deleting all of the objects causes their destructor to be called which will finish sending any
 	unsent information.*/
-	m_newClientList.RemoveAll();
+	m_clientMessages.clear();
 }
 
-void Server::BroadCastMessage(const std::string& msg)
+void Server::BroadCastMessage(const string& msg)
 {
-	//Need to implement.
-	/*for (auto it = m_newClientList.Begin(); it != m_newClientList.End(); it++)
+	lock_guard<mutex> lck(m_pairMutex);
+
+	for (int i = 0; i < m_clientMessages.size(); i++)
 	{
-		
-	}*/
+		m_clientMessages[i].second.push_back(msg);
+	}
 }
 
-bool Server::AddClient(ConnectionInfo& ci, const std::string& username)
+void Server::RecordMessages(ClientAndMessageDeque* cm)
 {
-	return m_newClientList.Add(move(ci), username);
+	string username = cm->first.GetUsername();
+
+	while (cm->second.size() > 0)
+	{
+		WriteMsgToDb(username, cm->second.front());
+		cm->second.pop_front();
+	}
+}
+
+void Server::WriteMsgToDb(const string& username, const string& msg)
+{
+	lock_guard<mutex> lck(m_msgDbMutex);
+
+	switch (MessageBase::GetMessageType(msg))
+	{
+		case message_t::MESSAGE:
+			{
+				MessageToUser mtu = MessageToUser::ParseMessageToUser(msg);
+				string query{ "INSERT INTO " };
+				query += username + COMMON_TABLE_NAME;
+				query += " VALUES(";
+				query += TO_COLUMN;
+				query += "='" + mtu.To() + "',";
+				query += FROM_COLUMN;
+				query += "='" + mtu.From() + "',";
+				query += MSG_COLUMN;
+				query += "='" + mtu.Message() + "',";
+				query += DATE_COLUMN;
+				query += "='" + mtu.Date() + "',";
+				query += STATUS_COLUMN;
+				query += "='" + mtu.Status() + "');";
+
+				m_messageDb.ExecuteStatement(query);
+			}
+			break;
+	}
+}
+
+void Server::ProcessIncomingMessages()
+{
+	m_pairMutex.lock();
+	for (auto it = m_clientMessages.begin(); it != m_clientMessages.end(); it++)
+	{
+		m_pairMutex.unlock();
+		string msg;
+
+		RecvMsg(it, msg);
+		m_pairMutex.lock();
+	}
+	m_pairMutex.unlock();
+}
+
+void Server::SendQueuedMessages()
+{
+	m_pairMutex.lock();
+	for (auto it = m_clientMessages.begin(); it != m_clientMessages.end(); it++)
+	{
+		m_pairMutex.unlock();
+		while (it->second.size() > 0)
+		{
+			SendMsg(it, it->second.front());
+			it->second.pop_front();
+		}
+		m_pairMutex.lock();
+	}
+	m_pairMutex.unlock();
+}
+
+string Server::BuildUnsentMessagesQuery(const string& to)
+{
+	string query = "SELECT ";
+	query += FROM_COLUMN;
+	query += ",";
+	query += MSG_COLUMN;
+	query += ",";
+	query += DATE_COLUMN;
+	query += " FROM ";
+	query += to;
+	query += COMMON_TABLE_NAME;
+	query += " WHERE ";
+	query += TO_COLUMN;
+	query += "='";
+	query += to;
+	query += "' AND ";
+	query += STATUS_COLUMN;
+	query += "='";
+	query += UNSENT_STATUS;
+	query += "';";
+
+	return query;
+}
+
+string Server::BuildMessage(const string& from, const string& to, const string& message, const string& date)
+{
+	string msg = MSG_BTWN_USERS;
+	msg += DELIMITER;
+	msg += FROM_FIELD;
+	msg += from;
+	msg += DELIMITER;
+	msg += TO_FIELD;
+	msg += to;
+	msg += DELIMITER;
+	msg += MESSAGE_FIELD;
+	msg += message;
+	msg += DELIMITER;
+	msg += DATE_FIELD;
+	msg += date;
+	msg += DELIMITER;
+	msg += END_OF_MSG;
+	return msg;
+}
+
+void Server::SendMsg(vector<ClientAndMessageDeque>::iterator cm, const string& msg)
+{
+	switch (cm->first.SendMsg(msg))
+	{
+		case ConnectionState::ERR:
+			cm->second.push_back(msg);
+			RecordMessages(cm._Ptr);
+			m_clientMessages.erase(cm);
+			break;
+		case ConnectionState::NOT_FULLY_SENT:
+			/*Place the rest of the unsent message in the message deque.*/
+			cm->second.push_front(msg.substr(cm->first.GetConnectionInfo()->BytesSent()));
+			break;
+		case ConnectionState::NOT_SENT:
+			cm->second.push_back(msg);
+			break;
+		case ConnectionState::WANT_READ:
+		case ConnectionState::WANT_WRITE:
+			cm->second.push_front(msg);
+			break;
+	}
+}
+
+void Server::RecvMsg(vector<ClientAndMessageDeque>::iterator cm, string& msgBuffer)
+{
+	ClientInfo* ci = &(cm->first);
+
+	switch (ci->ReceiveMsg(msgBuffer))
+	{
+		case ConnectionState::ERR:
+			/*Record any unsent messages.*/
+			RecordMessages(cm._Ptr);
+			/*Destroy the object.*/
+			m_clientMessages.erase(cm);
+			break;
+		case ConnectionState::SHUTDOWN:
+			/*Record any unsent messages.*/
+			RecordMessages(cm._Ptr);
+			/*Destroy the object.*/
+			m_clientMessages.erase(cm);
+			break;
+		case ConnectionState::RECEIVED:
+			/*Process the message. Must determine what information the client needs, if any.*/
+			switch (MessageBase::GetMessageType(msgBuffer))
+			{
+				case message_t::MESSAGE:
+				{
+					MessageToUser mtu = MessageToUser::ParseMessageToUser(msgBuffer);
+					if (mtu.From() != cm->first.GetUsername())
+					{
+						cm->first.SendMsg(INVALID_MSG);
+					}
+					else
+					{
+						vector<ClientAndMessageDeque>::iterator it = FindUserInQueue(mtu.To());
+						if (it != m_clientMessages.end())
+						{
+							it->second.push_back(BuildMessage(mtu));
+						}
+						else if (FindUserInDb(mtu.To()))
+						{
+							/*Build query to add the message to mtu.To()'s table.*/
+							string query{"INSERT INTO "};
+							query += mtu.To() + TABLE_NAME;
+							query += "(";
+							query += TO_COLUMN;
+							query += ",";
+							query += FROM_COLUMN;
+							query += ",";
+							query += MSG_COLUMN;
+							query += ",";
+							query += DATE_COLUMN;
+							query += ",";
+							query += STATUS_COLUMN;
+							query += ") VALUES(";
+							query += mtu.To() + "," + mtu.From() + "," + mtu.Message() + "," + mtu.Date() + "," + mtu.Status();
+							query += ");";
+
+							m_messageDb.ExecuteStatement(query);
+						}
+						else
+						{
+							string msg = USER_DOESNT_EXIST;
+							msg += DELIMITER;
+							msg += USERNAME_FIELD;
+							msg += mtu.To();
+							cm->first.SendMsg(msg);
+						}
+					}
+				}
+				break;
+				case message_t::REQUEST_CONVO_LISTS:
+				{
+					/*
+					*
+					*
+					*		STOPPED
+					*		HERE
+					*
+					*
+					*/
+					RequestConversationLists rcl = RequestConversationLists::ParseRequest(msgBuffer);
+				}
+				break;
+				case message_t::REQUEST_CONVO_MSGS:
+				{
+					RequestConversationMsgs rcm = RequestConversationMsgs::ParseRequest(msgBuffer);
+				}
+				break;
+				case message_t::LOGOUT:
+				{
+					LogoutMessage lm = LogoutMessage::ParseMessage(msgBuffer);
+				}
+				break;
+				case message_t::INVALID:
+				{
+					cm->second.push_back(INVALID_MSG_RESPONSE);
+				}
+				break;
+				case message_t::REQUEST_FILE:
+				{
+					FileRequestMessage frm = FileRequestMessage::ParseMessage(msgBuffer);
+				}
+				break;
+				case message_t::REQUEST_USER_INFO:
+				{
+					UserInfoRequest uir = UserInfoRequest::ParseMessage(msgBuffer);
+				}
+				break;
+			}
+			break;
+		case ConnectionState::NO_DATA_PRESENT:
+			/*Do nothing.*/
+			break;
+		case ConnectionState::WANT_WRITE:
+		case ConnectionState::WANT_READ:
+			/*Do nothing.*/
+			break;
+	}
+}
+
+bool Server::AddClient(ClientInfo&& ci)
+{
+	string username = ci.GetUsername();
+	lock_guard<mutex> lck(m_pairMutex);
+
+	for (auto it = m_clientMessages.begin(); it != m_clientMessages.end(); it++)
+	{
+		if (username == it->first.GetUsername())
+			return false;
+	}
+
+	//m_clientMessages.push_back(ClientMessages{ move(ci), queue<string>{} });
+
+	/*Need to fill the queue with any unsent messages.*/
+	deque<string> unsentMessages{};
+
+	m_msgDbMutex.lock();
+	{
+		m_messageDb.ExecuteStatement(BuildUnsentMessagesQuery(username));
+		
+		while (m_messageDb.HasRows())
+		{
+			unsentMessages.push_back(BuildMessage(m_messageDb.GetColumnTxt(0), username, m_messageDb.GetColumnTxt(1), m_messageDb.GetColumnTxt(2)));
+			m_messageDb.StepNextRow();
+		}
+	}
+	m_msgDbMutex.unlock();
+
+	m_clientMessages.push_back(ClientAndMessageDeque{ move(ci), unsentMessages });
+
+	return true;
 }
 
 void Server::Run(ServerState state)
 {
-	lock_guard<mutex> lck(m_runMetx);
+	if (!m_runMetx.try_lock())
+		return;
 
 	SetState(state);
 	while (state != ServerState::OFF)
@@ -45,48 +340,8 @@ void Server::Run(ServerState state)
 		switch (state)
 		{
 			case ServerState::RUNNING:
-				for (auto it = m_newClientList.Begin(); it != m_newClientList.End(); it++)
-				{
-					ConnectionInfo* ci = it->GetConnectionInfo();
-					string msg;
-					
-					switch (ci->Receive(msg))
-					{
-						case ConnectionState::ERR:
-							/*Destroy the object.*/
-							m_newClientList.Erase(it);
-							break;
-						case ConnectionState::SHUTDOWN:
-							/*Destroy the object.*/
-							m_newClientList.Erase(it);
-							break;
-						case ConnectionState::RECEIVED:
-							/*Begin processing the message. Must determine what information the client needs, if any.*/
-							switch (ParseMessage(msg))
-							{
-								case message_t::MESSAGE:
-									break;
-								case message_t::REQUEST_CONVO_LISTS:
-									break;
-								case message_t::REQUEST_CONVO_MSGS:
-									break;
-								case message_t::LOGIN:
-									break;
-								case message_t::LOGOUT:
-									break;
-								case message_t::INVALID:
-									break;
-							}
-							break;
-						case ConnectionState::NO_DATA_PRESENT:
-							/*Do nothing.*/
-							break;
-						case ConnectionState::WANT_READ:
-							/*As per OpenSSL's documentation, we must call the function again.*/
-							ci->Receive(msg);
-							break;
-					}
-				}
+				ProcessIncomingMessages();
+				SendQueuedMessages();
 				break;
 			case ServerState::NOT_ACCEPTING_CONNECTIONS:
 				DisconnectClients();
@@ -100,6 +355,8 @@ void Server::Run(ServerState state)
 
 		state = GetState();
 	}
+
+	m_runMetx.unlock();
 }
 
 inline void Server::SetState(ServerState state)
@@ -114,5 +371,5 @@ inline ServerState Server::GetState()
 
 int Server::NumberOfClients()
 {
-	return m_newClientList.Size();
+	return m_clientMessages.size();
 }
--- BlueSOD/Server.h
+++ BlueSOD/Server.h
@@ -2,25 +2,17 @@
 #include <mutex>
 #include <string>
 #include <list>
+#include <utility>
+#include <deque>
+#include <vector>
 #include "ServerConnections.h"
 #include "ServerConcurrency.h"
 #include "SQLiteDB.h"
-#include "ClientInfoList.h"
+#include "ClientInfo.h"
 #include "Messages.h"
-
-#define BEGIN_MESSAGE "{beg}"
-#define END_MESSAGE "{end}"
-#define IMAGE_MESSAGE "{image}"
-#define END_IMAGE_MESSAGE "{/image}"
 
 #define MSG_DB_NAME "MessageDatabase"
 #define TABLE_NAME "Conversations"
-
-enum class message_t
-{
-	MESSAGE, REQUEST_CONVO_LISTS, REQUEST_CONVO_MSGS, LOGIN, LOGOUT, INVALID
-};
-
 
 //This class represents the Server. The ServerManager will handle any initial incoming connections
 //and will pass it off to the Server, which lies in its own thread. The Server then will handle
@@ -30,11 +22,12 @@ enum class message_t
 class Server
 {
 private:
-	//List of clients currently connected.
-	ClientInfoList m_newClientList{};
+	typedef std::pair<ClientInfo, std::deque<std::string>> ClientAndMessageDeque;
+	std::vector<ClientAndMessageDeque> m_clientMessages;
+	std::mutex m_pairMutex;
 	//The state of the Server.
 	ThreadSafe<ServerState> m_state{ ServerState::OFF };
-	SQLiteDb* m_messageDb;
+	SQLiteDb m_messageDb;
 	std::mutex m_msgDbMutex;
 	std::mutex m_runMetx;
 
@@ -47,10 +40,8 @@ public:
 	Server() = default;
 	~Server() = default;
 	
-	bool AddClient(ConnectionInfo& ci, const std::string& username);
-	//Initializes the server. This is where the code for handling communication will be.
-	//TO DO: Implement.
-	void Run(ServerState state = ServerState::RUNNING);
+	inline bool AddClient(ClientInfo&& ci);
+	void Run(ServerState state = ServerState::OFF);
 	//Sets the state of the Server.
 	inline void SetState(ServerState state);
 	inline ServerState GetState();
@@ -58,8 +49,17 @@ public:
 
 private:
 	void DisconnectClients();
-	/*Not finished.*/
 	void BroadCastMessage(const std::string& msg);
-	message_t ParseMessage(std::string& msg);
+	void RecordMessages(ClientAndMessageDeque* cm);
+	void WriteMsgToDb(const std::string& username, const std::string& msg);
+	void ProcessIncomingMessages();
+	void SendQueuedMessages();
+	std::string BuildUnsentMessagesQuery(const std::string& to);
+	std::string BuildMessage(const std::string& from, const std::string& to, const std::string& message, const std::string& date);
+	std::string BuildMessage(const MessageToUser& mtu);
+	void SendMsg(std::vector<ClientAndMessageDeque>::iterator cm, const std::string& msg);
+	void RecvMsg(std::vector<ClientAndMessageDeque>::iterator cm, std::string& msgBuffer);
+	std::vector<ClientAndMessageDeque>::iterator FindUserInQueue(const std::string& username);
+	bool FindUserInDb(const std::string& user);
 };
 
