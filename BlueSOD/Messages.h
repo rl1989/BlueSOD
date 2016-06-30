adacd591435777d1646b39a4a1b7691f310ecf4a
Ricky <ricky2011@knights.ucf.edu>
2016-06-30 02:38:38 -0400

    Changed Message to MessageBase. Made the classes for the different types of messages from the client.

--- BlueSOD/Messages.h
+++ BlueSOD/Messages.h
@@ -1,4 +1,160 @@
 #pragma once
 #include <string>
 
-const std::string RESET_CONNECTION = "";
+/*Message codes*/
+#define REJECTED_LOGIN_MSG     "303"
+#define SUCCESSFUL_LOGIN_MSG   "301"
+#define UNSUCCESSFUL_LOGIN_MSG "302"
+#define LOGIN_MSG              "300"
+#define LOGOUT_MSG             "304"
+#define INVALID_MSG            "999"
+#define INVALID_MSG_RESPONSE   "990"
+#define MSG_BTWN_USERS         "100"
+#define REQ_CONVO_LISTS        "201"
+#define REQ_CONVO_MSGS         "202"
+#define REQ_FILE               "203"
+#define REQ_USER_INFO          "204"
+#define USER_DOESNT_EXIST      "901"
+
+/*Miscellaneous message entities*/
+#define DELIMITER ";"
+#define END_OF_MSG "{e}"
+
+/*Message fields*/
+#define PASSWORD_FIELD "p:"
+#define FROM_FIELD     "f:"
+#define TO_FIELD       "t:"
+#define MESSAGE_FIELD  "m:"
+#define USERNAME_FIELD "u:"
+#define DATE_FIELD     "d:"
+#define FOR_FIELD      "f:"
+#define BTWN_FIELD     "b:"
+#define FILENAME_FIELD "f:"
+#define TARGET_FIELD   "t:"
+#define STATUS_FIELD   "s:"
+
+enum class message_t
+{
+	MESSAGE, REQUEST_CONVO_LISTS, REQUEST_CONVO_MSGS, LOGOUT, INVALID, REQUEST_FILE, REQUEST_USER_INFO, LOGIN
+};
+
+class UserInfoRequest : public MessageBase
+{
+private:
+	std::string m_for;
+	std::string m_target;
+
+	UserInfoRequest(const std::string& _for, const std::string& target);
+	UserInfoRequest() = default;
+public:
+	inline std::string For();
+	inline std::string Target();
+
+	static UserInfoRequest ParseMessage(const std::string& msg);
+};
+
+class FileRequestMessage : public MessageBase
+{
+private:
+	std::string m_for;
+	std::string m_filename;
+
+	FileRequestMessage(const std::string& _for, const std::string& filename);
+	FileRequestMessage() = default;
+public:
+	inline std::string For();
+	inline std::string Filename();
+
+	static FileRequestMessage ParseMessage(const std::string& msg);
+};
+
+class LogoutMessage : public MessageBase
+{
+private:
+	std::string m_username;
+
+	explicit LogoutMessage(const std::string& username);
+	LogoutMessage() = default;
+public:
+	inline std::string Username();
+
+	static LogoutMessage ParseMessage(const std::string& msg);
+};
+
+class RequestConversationMsgs : public MessageBase
+{
+private:
+	std::string m_for;
+	std::string m_to;
+
+	RequestConversationMsgs(const std::string& _for, const std::string& to);
+	RequestConversationMsgs() = default;
+public:
+	inline std::string For();
+	inline std::string To();
+
+	static RequestConversationMsgs ParseRequest(const std::string& msg);
+};
+
+class RequestConversationLists : public MessageBase
+{
+private:
+	std::string m_for;
+	
+	explicit RequestConversationLists(const std::string& _for);
+	RequestConversationLists() = default;
+public:
+	inline std::string For();
+
+	static RequestConversationLists ParseRequest(const std::string& msg);
+};
+
+class MessageToUser : public MessageBase
+{
+private:
+	std::string m_from;
+	std::string m_to;
+	std::string m_message;
+	std::string m_date;
+	std::string m_status;
+
+	MessageToUser(const std::string& from, const std::string& to, const std::string& message, const std::string& date, const std::string& status);
+	MessageToUser() = default;
+public:
+	inline std::string From();
+	inline std::string To();
+	inline std::string Message();
+	inline std::string Date();
+	inline std::string Status();
+
+	static MessageToUser ParseMessageToUser(const std::string& msg);
+};
+
+class LoginMessage : public MessageBase
+{
+private:
+	std::string m_username;
+	std::string m_password;
+
+	LoginMessage(const std::string& username, const std::string& password);
+	LoginMessage() = default;
+public:
+	inline std::string Username();
+	inline std::string Password();
+
+	static LoginMessage ParseLoginMsg(const std::string& msg);
+};
+
+class MessageBase
+{
+private:
+	message_t m_code;
+	bool m_valid;
+public:
+	static message_t GetMessageType(const std::string& msg);
+
+	inline bool IsValid();
+	inline message_t Code();
+protected:
+	MessageBase(message_t code = message_t::INVALID, bool isValid = false);
+};
