49c9a4f47135ad324c2e28eceb4861b67a7eeba3
Ricky <ricky2011@knights.ucf.edu>
2016-07-18 02:49:46 -0400

    Added an ID field and modified the constructor to accept one.

--- BlueSOD/ClientInfo.cpp
+++ BlueSOD/ClientInfo.cpp
@@ -4,11 +4,32 @@ using std::chrono::steady_clock;
 using std::move;
 using std::string;
 
+ClientInfo::ClientInfo(const ConnectionInfo& ci, const string& username, int id)
+	: m_connectionInfo{ci},
+	m_username{username},
+	m_id{id}
+{}
+
 ClientInfo::ClientInfo(ConnectionInfo&& ci, const string& username, int id)
 	:m_connectionInfo{ move(ci) },
 	m_username{ username },
 	m_id{ id }
 {}
+
+ClientInfo::ClientInfo(const ClientInfo& ci)
+	: m_connectionInfo{ci.m_connectionInfo},
+	m_username{ci.m_username},
+	m_id{ci.m_id}
+{}
+
+ClientInfo& ClientInfo::operator=(const ClientInfo& ci)
+{
+	m_connectionInfo = ci.m_connectionInfo;
+	m_username = ci.m_username;
+	m_id = ci.m_id;
+
+	return *this;
+}
 
 ClientInfo::ClientInfo(ClientInfo&& move)
 	: m_username{move.GetUsername()},
@@ -34,7 +55,17 @@ const std::string& ClientInfo::GetUsername()
 	return m_username;
 }
 
+const std::string & ClientInfo::GetUsername() const
+{
+	return m_username;
+}
+
 int ClientInfo::GetId()
+{
+	return m_id;
+}
+
+int ClientInfo::GetId() const
 {
 	return m_id;
 }
@@ -44,7 +75,17 @@ ConnectionInfo* ClientInfo::GetConnectionInfo()
 	return &m_connectionInfo;
 }
 
+const ConnectionInfo* ClientInfo::GetConnectionInfo() const
+{
+	return &m_connectionInfo;
+}
+
 std::chrono::time_point<std::chrono::steady_clock> ClientInfo::LastAccessed()
+{
+	return m_lastAccessed;
+}
+
+std::chrono::time_point<std::chrono::steady_clock> ClientInfo::LastAccessed() const
 {
 	return m_lastAccessed;
 }
@@ -61,7 +102,12 @@ ConnectionState ClientInfo::SendMsg(const std::string& msg)
 
 ConnectionState ClientInfo::ReceiveMsg(std::string& msg)
 {
-	return m_connectionInfo.Receive(msg);
+	ConnectionState state = m_connectionInfo.Receive(msg);
+
+	if (state == ConnectionState::RECEIVED)
+		m_lastAccessed = steady_clock::now();
+
+	return state;
 }
 
 void ClientInfo::SetUsername(const std::string& username)
--- BlueSOD/ClientInfo.h
+++ BlueSOD/ClientInfo.h
@@ -13,15 +13,22 @@ private:
 	ConnectionInfo m_connectionInfo{};
 	std::chrono::time_point<std::chrono::steady_clock> m_lastAccessed{ std::chrono::steady_clock::now() };
 public:
+	ClientInfo(const ConnectionInfo& ci, const std::string& username, int id);
 	ClientInfo(ConnectionInfo&& ci, const std::string& username, int id);
 	~ClientInfo() = default;
+	ClientInfo(const ClientInfo& ci);
+	ClientInfo& operator=(const ClientInfo& ci);
 	ClientInfo(ClientInfo&& move);
 	ClientInfo& operator=(ClientInfo&& move);
 
 	const std::string& GetUsername();
+	const std::string& GetUsername() const;
 	int GetId();
+	int GetId() const;
 	ConnectionInfo* GetConnectionInfo();
+	const ConnectionInfo* GetConnectionInfo() const;
 	std::chrono::time_point<std::chrono::steady_clock> LastAccessed();
+	std::chrono::time_point<std::chrono::steady_clock> LastAccessed() const;
 	void CloseConnection();
 	ConnectionState SendMsg(const std::string& msg);
 	ConnectionState ReceiveMsg(std::string& msg);
