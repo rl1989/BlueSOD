8686d79ad16b227a99273bade7c81b1f80ccd8cd
Ricky <ricky2011@knights.ucf.edu>
2016-07-18 03:11:11 -0400

    Completely rewritten to use ThreadSafeQueues in the form of Queue, OutMsgQueue, IncMsgQueue, MsgQueues and ClientAndQueues. Also works with MessageBase derivatives in order to easily process Messages.

--- BlueSOD/Server.cpp
+++ BlueSOD/Server.cpp
@@ -1,6 +1,12 @@
 #pragma once
 #include "Server.h"
 
+using std::move;
+using std::string;
+using std::thread;
+using std::vector;
+using std::async;
+
 #define COMMON_TABLE_NAME "Messages"
 #define TO_COLUMN         "To"
 #define FROM_COLUMN       "From"
@@ -15,6 +21,257 @@
 #define DATE_COL_POS   3
 #define STATUS_COL_POS 4
 
+void client_handler(ClientInfo* ci, ThreadSafe<ServerState>* ts_state, IncMsgQueue* inc, OutMsgQueue* out)
+{
+	ServerState state = ts_state->RetrieveObject();
+
+	while (ci->GetConnectionInfo()->IsValid() && state != ServerState::OFF && state != ServerState::RESET)
+	{
+		string msg;
+		
+		switch (ci->ReceiveMsg(msg))
+		{
+			case ConnectionState::RECEIVED:
+				switch (MessageBase::GetMessageType(msg))
+				{
+					case message_t::MESSAGE_TO_USER:
+						inc->push(MessageToUser::ParseMessageToUser(msg));
+						break;
+					case message_t::LOGIN:
+						inc->push(LoginMessage::ParseLoginMsg(msg));
+						break;
+					case message_t::LOGOUT:
+						inc->push(LogoutMessage::ParseMessage(msg));
+						break;
+					case message_t::REQUEST_CONVO_LISTS:
+						inc->push(RequestConversationListMessage::ParseRequest(msg));
+						break;
+					case message_t::REQUEST_CONVO_MSGS:
+						inc->push(RequestConversationMsgs::ParseRequest(msg));
+						break;
+					case message_t::REQUEST_FILE:
+						inc->push(FileRequestMessage::ParseMessage(msg));
+						break;
+					case message_t::REQUEST_USER_INFO:
+						inc->push(UserInfoRequest::ParseMessage(msg));
+						break;
+					case message_t::INVALID:
+						inc->push(new InvalidMessage{});
+						break;
+				}
+				break;
+			case ConnectionState::SHUTDOWN:
+				ci->CloseConnection();
+				break;
+			case ConnectionState::WANT_READ:
+			case ConnectionState::NO_DATA_PRESENT:
+				break;
+			case ConnectionState::ERR:
+			default:
+				ci->CloseConnection();
+				break;
+		}
+
+		if (!out->empty())
+		{
+			MessageBase* nextMsg = out->front();
+
+			switch (nextMsg->Code())
+			{
+				case message_t::MESSAGE_TO_USER:
+					msg = ((MessageToUser*)nextMsg)->MakeMessage();
+					break;
+				case message_t::LOGIN:
+					msg = ((LoginMessage*)nextMsg)->MakeResponse();
+					break;
+				case message_t::LOGOUT:
+					msg = ((LogoutMessage*)nextMsg)->MakeResponse();
+					break;
+				case message_t::CONVERSATION_LIST:
+					
+					break;
+				case message_t::CONVERSATION_MSGS:
+					break;
+				case message_t::FILE:
+					break;
+				case message_t::USER_INFO:
+					break;
+			}
+			switch (ci->SendMsg(msg))
+			{
+				case ConnectionState::SENT:
+					out->pop();
+					delete nextMsg;
+					break;
+				case ConnectionState::SHUTDOWN:
+					ci->CloseConnection();
+					break;
+				case ConnectionState::ERR:
+					ci->CloseConnection();
+					break;
+				default:
+					break;
+			}
+		}
+
+		state = ts_state->RetrieveObject();
+	}
+
+	/*This won't do anything if ci is Invalid, but must be done if the server is off or resetting.*/
+	ci->CloseConnection();
+}
+
+Server::Server(ThreadSafeList<std::pair<int, Server*>>* master_list)
+	: m_masterList{master_list}
+{}
+
+bool Server::AddClient(const ClientInfo& ci)
+{
+	for (auto it = m_masterList->begin(); it != m_masterList->end(); it++)
+	{
+		/*user is already logged in*/
+		if (it->first == ci.GetId())
+			return false;
+	}
+	
+	m_clientList.push_back(ClientAndQueues{ ci, MsgQueues{} });
+
+	auto out = m_clientList.end() - 1;
+	/*Fill the outgoing messages queue*/
+	async(this->SearchForMessages, out->second.second, ci.GetId(), ci.GetUsername());
+	/*Allow a new thread to listen for any communication*/
+	m_receiverThreads.push_back(thread{ client_handler, &(out->first), &m_state, &(out->second.first), &(out->second.second) });
+
+	return true;
+}
+
+void Server::Run(ServerState state)
+{
+	lock_guard<mutex> lck(m_RunMutex);
+
+	SetState(state);
+	
+	while (state != ServerState::OFF)
+	{
+		/*Used in ServerState::RUNNING case*/
+		string msg;
+
+		switch (state)
+		{
+			case ServerState::START_UP:
+				/*Reserve space to increase performance later on*/
+				m_clientList.reserve(DEFAULT_VECTOR_SIZE);
+				m_receiverThreads.reserve(DEFAULT_VECTOR_SIZE);
+				SetState(ServerState::RUNNING);
+				break;
+			case ServerState::RUNNING:
+				/*Process messages*/
+				for (auto it = m_clientList.begin(); it != m_clientList.end(); it++)
+				{
+					string username = it->first.GetUsername();
+					IncMsgQueue& inc = it->second.first;
+					/*True if inc was erased so is invalid. This will break the while loop so that !inc.empty()
+					isn't evaluated again.*/
+					bool erased = false;
+
+					/*Process incoming messages*/
+					while (!inc.empty() && !erased)
+					{
+						MessageBase* message = inc.front();
+
+						switch (message->Code())
+						{
+							case message_t::LOGOUT:
+								{
+									/*Close the connection*/
+									it->first.CloseConnection();
+									/*Save the outgoing message queue*/
+									async(this->SaveMessages, move(it->second.second));
+									/*Remove this client from the master list and this Server's list*/
+									async(this->RemoveFromMasterList, it->first.GetId());
+									m_clientList.erase(it);
+									/*Logout is the only one that doesn't get deleted in an async method.*/
+									delete message;
+
+									erased = true;
+								}
+								break;
+							case message_t::MESSAGE_TO_USER:
+								{
+									MessageToUser* mtu = dynamic_cast<MessageToUser*>(message);
+									/*Either add the message to the To user's OutMsgQueue, record the message in the
+									  To user's messages table, or add an invalid message to the From user's OutMsgQueue.*/
+									async(this->ProcessMessageToUser, mtu);
+								}
+								break;
+							case message_t::REQUEST_CONVO_LISTS:
+								{
+									RequestConversationListMessage* rcl = dynamic_cast<RequestConversationListMessage*>(message);
+									OutMsgQueue* queue = &it->second.second;
+									/*Find the conversations and add it to the client's OutMsgQueue.*/
+									async(this->FindConversationList, rcl, queue);
+								}
+								break;
+							case message_t::REQUEST_CONVO_MSGS:
+								{
+									RequestConversationMsgs* rcm = dynamic_cast<RequestConversationMsgs*>(message);
+									OutMsgQueue* queue = &it->second.second;
+									/*Find the messages between rcm->Requester() and rcm->Requested() and add
+									  them to the client's queue.*/
+									async(this->FindConversation, rcm, queue);
+								}
+								break;
+							case message_t::REQUEST_FILE:
+								{
+									FileRequestMessage* frm = dynamic_cast<FileRequestMessage*>(message);
+									OutMsgQueue* queue = &it->second.second;
+									/*Attempt to locate the file and add it to the client's queue.*/
+									async(this->FindFile, frm, queue);
+								}
+								break;
+							case message_t::REQUEST_USER_INFO:
+								{
+									UserInfoRequest* uir = dynamic_cast<UserInfoRequest*>(message);
+									OutMsgQueue* queue = &it->second.second;
+									/*Attempt to locate the information on the user and add it to the client's queue.*/
+									async(this->FindUserInformation, uir, queue);
+								}
+								break;
+						}
+						
+						/*We're finished with the message, so remove it from the queue.*/
+						if (!erased)
+						{
+							inc.pop();
+						}
+					}
+				}
+				break;
+			case ServerState::RESET:
+				/*We need to make sure that the state stays at RESET until this process is finished.*/
+				m_state.LockChange(ServerState::RESET);
+
+				/*We are waiting until every thread gets the message and joins.*/
+				for (auto it = m_receiverThreads.begin(); it != m_receiverThreads.end(); it++)
+				{
+					if (it->joinable())
+						it->join();
+				}
+
+				/*Now, unallocate the memory that was used.*/
+				m_receiverThreads.clear();
+				m_clientList.clear();
+
+				m_state.Unlock();
+
+				SetState(ServerState::START_UP);
+				break;
+		}
+
+		state = GetState();
+	}
+}
+
 inline void Server::SetState(ServerState state)
 {
 	m_state.ChangeObject(state);
@@ -27,5 +284,218 @@ inline ServerState Server::GetState()
 
 int Server::NumberOfClients()
 {
-	return m_clientMessages.size();
+	return m_clientList.size();
+}
+
+void AddToQueue(Queue& queue, const string& msg)
+{
+	switch (MessageBase::GetMessageType(msg))
+	{
+		case message_t::INVALID:
+			queue.push(new InvalidMessage{});
+			break;
+		case message_t::LOGOUT:
+			queue.push(LogoutMessage::ParseMessage(msg));
+			break;
+		case message_t::MESSAGE_TO_USER:
+			queue.push(MessageToUser::ParseMessageToUser(msg));
+			break;
+		case message_t::REQUEST_CONVO_LISTS:
+			queue.push(RequestConversationListMessage::ParseRequest(msg));
+			break;
+		case message_t::REQUEST_CONVO_MSGS:
+			queue.push(RequestConversationMsgs::ParseRequest(msg));
+			break;
+		case message_t::REQUEST_FILE:
+			queue.push(FileRequestMessage::ParseMessage(msg));
+			break;
+		case message_t::REQUEST_USER_INFO:
+			queue.push(UserInfoRequest::ParseMessage(msg));
+			break;
+	}
+}
+
+void Server::SaveMessages(ThreadSafeQueue<MessageBase*>&& messages)
+{
+	while (messages.size() > 0)
+	{
+		MessageBase* msg = messages.front();
+		if (msg->Code() == message_t::MESSAGE_TO_USER)
+		{
+			m_msgDbMutex.lock();
+			m_messageDb.ExecuteStatement(msg->MakeQueries());
+			m_msgDbMutex.unlock();
+		}
+		messages.pop();
+	}
+}
+
+void Server::SearchForMessages(OutMsgQueue& messages, int userId, const string& username)
+{
+	/*We need to build a query, execute the query, parse the results, and add them to messages*/
+	string query = "SELECT message FROM ";
+	query += SQLiteDb::MessagesTableName(username) + " WHERE id='";
+	query += userId;
+	query += "'";
+
+	m_msgDbMutex.lock();
+	if (m_messageDb.ExecuteStatement(query) == SQLITE_ROW)
+	{
+		while (m_messageDb.HasRows())
+		{
+			string msg = m_messageDb.GetColumnTxt(0);
+			AddToQueue(messages, msg);
+			m_messageDb.StepNextRow();
+		}
+	}
+	m_msgDbMutex.unlock();
+}
+
+void Server::ProcessMessageToUser(MessageToUser* mtu)
+{
+	bool found = false;
+
+	/*Search for the person the message is intended for.*/
+	for (auto it = m_masterList->begin(); it != m_masterList->end(); it++)
+	{
+		if (it->first == mtu->ToId())
+		{
+			/*Find the message recipient's outgoing queue and add the message to it.*/
+			for (auto iter = it->second->m_clientList.begin(); iter != it->second->m_clientList.end(); iter++)
+			{
+				if (mtu->ToId() == iter->first.GetId())
+				{
+					iter->second.second.push(mtu);
+					found = true;
+					break;
+				}
+			}
+			break;
+		}
+		if (it->first == mtu->FromId())
+		{
+
+		}
+	}
+
+	/*If the user isn't logged in, we'll record the message right away.
+	  NOTE: The message will be recorded in the database once the user logs out or their connection is severed.*/
+	if (!found)
+	{
+		string query = "SELECT Username FROM user.db WHERE Username='" + mtu->To() + "'";
+		m_msgDbMutex.lock();
+		if (m_messageDb.ExecuteStatement(query) == SQLITE_ROW)
+		{
+			m_messageDb.ExecuteStatement(mtu->MakeQueries());
+		}
+		m_msgDbMutex.unlock();
+	}
+
+	/*We're done with the message, so delete it. I don't delete it outside of this method, because it
+	  is intended to run asynchronously. The only time it makes sense to delete the pointer is when this
+	  method is finished with it.*/
+	delete mtu;
+}
+
+void Server::FindConversationList(RequestConversationListMessage* rcl, OutMsgQueue* queue)
+{
+	if (rcl == nullptr || queue == nullptr || rcl->IsValid() == false)
+		return;
+
+	vector<string> conversationList;
+	
+	m_messageDb.Lock();
+	/*SELECT * FROM rcl->Requester()Conversations LIMIT 15;*/
+	if (m_messageDb.ExecuteStatement(rcl->MakeQueries()) == SQLITE_ROW)
+	{
+		/*Load the data while there is data available.*/
+		while (m_messageDb.HasRows())
+		{
+			conversationList.push_back(m_messageDb.GetColumnTxt(0));
+			m_messageDb.StepNextRow();
+		}
+	}
+	m_messageDb.Unlock();
+
+	queue->push(new ConversationListMessage{ conversationList });
+	/*We're done with the message, so delete it. I don't delete it outside of this method, because it
+	is intended to run asynchronously. The only time it makes sense to delete the pointer is when this
+	method is finished with it.*/
+	delete rcl;
+}
+
+void Server::FindConversation(RequestConversationMsgs* rcm, OutMsgQueue* queue)
+{
+	if (rcm == nullptr || queue == nullptr || rcm->IsValid() == false)
+		return;
+
+	vector<MessageToUser> messages;
+
+	m_messageDb.Lock();
+	/*SELECT * FROM rcm->Requester()Conversations
+	  WHERE to='rcm->With()' OR from='rcm->With()' LIMIT 15;*/
+	if (m_messageDb.ExecuteStatement(rcm->MakeQueries()) == SQLITE_ROW)
+	{
+		while (m_messageDb.HasRows())
+		{
+			string from = m_messageDb.GetColumnTxt(0), 
+				   to = m_messageDb.GetColumnTxt(1), 
+				   message = m_messageDb.GetColumnTxt(2), 
+				   date = m_messageDb.GetColumnTxt(3), 
+				   status = m_messageDb.GetColumnTxt(4);
+			MessageToUser mtu = MessageToUser::MakeMessageToUser(from, to, message, date, status);
+
+			messages.push_back(move(mtu));
+
+			m_messageDb.StepNextRow();
+		}
+	}
+	m_messageDb.Unlock();
+
+	queue->push(new ConversationMessagesMessage{ rcm->With(), move(messages) });
+
+	/*We're done with the message, so delete it. I don't delete it outside of this method, because it
+	is intended to run asynchronously. The only time it makes sense to delete the pointer is when this
+	method is finished with it.*/
+	delete rcm;
+}
+
+void Server::FindFile(FileRequestMessage* frm, OutMsgQueue* queue)
+{
+	if (frm == nullptr || queue == nullptr || frm->IsValid() == false)
+		return;
+
+	m_messageDb.Lock();
+
+	m_messageDb.Unlock();
+	/*We're done with the message, so delete it. I don't delete it outside of this method, because it
+	is intended to run asynchronously. The only time it makes sense to delete the pointer is when this
+	method is finished with it.*/
+	delete frm;
+}
+
+void Server::FindUserInformation(UserInfoRequest* uir, OutMsgQueue* queue)
+{
+	if (uir == nullptr || queue == nullptr || uir->IsValid() == false)
+		return;
+
+	m_messageDb.Lock();
+
+	m_messageDb.Unlock();
+	/*We're done with the message, so delete it. I don't delete it outside of this method, because it
+	is intended to run asynchronously. The only time it makes sense to delete the pointer is when this
+	method is finished with it.*/
+	delete uir;
+}
+
+void Server::RemoveFromMasterList(int id)
+{
+	for (auto iter = m_masterList->begin(); iter != m_masterList->end(); iter++)
+	{
+		if (iter->first == id)
+		{
+			m_masterList->erase(iter);
+			break;
+		}
+	}
 }
--- BlueSOD/Server.h
+++ BlueSOD/Server.h
@@ -6,6 +6,8 @@
 #include <deque>
 #include <vector>
 #include <set>
+#include <thread>
+#include <future>
 #include "ServerConnections.h"
 #include "ServerConcurrency.h"
 #include "SQLiteDB.h"
@@ -13,47 +15,48 @@
 #include "Messages.h"
 #include "TS_Queue.h"
 #include "ThreadSafeVector.h"
+#include "ThreadSafeList.h"
 
 #define MSG_DB_NAME "MessageDatabase"
 #define TABLE_NAME "ConversationsTable"
 #define USER_TABLE "UsersTable"
 #define UT_USERNAME "Username"
 
-/*The MessageDEQueue is a double ended queue of strings. It is a double ended queue of strings because incoming messages
- are placed at the end and outgoing messages are from the front.*/
-typedef typename ThreadSafeQueue<MessageBase> IncMsgQueue;
-typedef typename ThreadSafeQueue<MessageBase> OutMsgQueue;
+/*
+	These represent the different queues a connection will use to store and retrieve messages.
+	An OutMsgQueue holds messages the server needs to send to the client. The IncMsgQueue is the
+	queue a client places incoming messages.
+*/
+typedef ThreadSafeQueue<MessageBase*> Queue;
+typedef Queue OutMsgQueue;
+typedef Queue IncMsgQueue;
 typedef std::pair<IncMsgQueue, OutMsgQueue> MsgQueues;
 typedef std::pair<ClientInfo, MsgQueues> ClientAndQueues;
-/*The ClientAndMessageQueue is a pair of ClientInfo and MessageDEQueue, effectively associating a ClientInfo and a 
-  MessageDEQueue*/
 
 //This class represents the Server. The ServerManager will handle any initial incoming connections
 //and will pass it off to the Server, which lies in its own thread. The Server then will handle
-//the communication between clients. This design allows the Server to handle only one type of
-//communication.
-//TO DO: Almost everything.
+//the communication between clients.
 class Server
 {
 private:
-	ThreadSafeVector<ClientAndQueues> m_clientMessages;
-	std::mutex m_pairMutex;
+	static const int DEFAULT_VECTOR_SIZE = 20;
+	ThreadSafeVector<ClientAndQueues> m_clientList{DEFAULT_VECTOR_SIZE};
 	//The state of the Server.
 	ThreadSafe<ServerState> m_state{ ServerState::OFF };
-	SQLiteDb m_messageDb;
-	std::mutex m_msgDbMutex;
-	std::mutex m_runMetx;
+	SQLiteDb m_messageDb{};
+	std::mutex m_msgDbMutex{};
+	/*Restricts Run(1) to be called one at a time.*/
+	std::mutex m_RunMutex{};
+	std::vector<std::thread> m_receiverThreads{DEFAULT_VECTOR_SIZE};
 
-	/*static SQLiteDb* m_msgDb;
-	static int NUMBER_OF_SERVERS;
-	static std::shared_mutex DB_MUTEX;*/
-
+	ThreadSafeList<std::pair<int, Server*>>* m_masterList{nullptr};
 
 public:
 	Server() = default;
 	~Server() = default;
+	Server(ThreadSafeList<std::pair<int,Server*>>* master_list);
 	
-	inline bool AddClient(ClientInfo&& ci);
+	bool AddClient(const ClientInfo& ci);
 	void Run(ServerState state = ServerState::OFF);
 	//Sets the state of the Server.
 	inline void SetState(ServerState state);
@@ -61,5 +64,27 @@ public:
 	int NumberOfClients();
 
 private:
-};
+	void SaveMessages(ThreadSafeQueue<MessageBase*>&& messages);
+	void SearchForMessages(OutMsgQueue& messages, int userId, const std::string& username);
 
+	/*
+		Process the different incoming request messages. Each function is intended to run asynchronously. Because
+		the pointer being passed in can potentially be used longer than the calling code lasts, each function will
+		be tasked with deleting the pointer passed in.
+	*/
+	/*Send the message from From() to To() if To() is a valid user. If the valid user is logged in, it will
+	  add it to its associated queue. If not, then the message will be recorded in the database for later use.*/
+	void ProcessMessageToUser(MessageToUser* mtu);
+	/*This will find all conversations associated with rcl->For() and add them to the queue.*/
+	void FindConversationList(RequestConversationListMessage* rcl, OutMsgQueue* queue);
+	/*This will find the messages associated with the conversation between rcm->Requester() and rcm->Requested()
+	  and add them to the client's queue.*/
+	void FindConversation(RequestConversationMsgs* rcm, OutMsgQueue* queue);
+	/*This will find the file being requested and add it to the client's queue.*/
+	void FindFile(FileRequestMessage* frm, OutMsgQueue* queue);
+	/*This will locate the information of the user uir->Target() and add it to the client's queue.*/
+	void FindUserInformation(UserInfoRequest* uir, OutMsgQueue* queue);
+
+	/*Removes the client from the master list.*/
+	void RemoveFromMasterList(int id);
+};
